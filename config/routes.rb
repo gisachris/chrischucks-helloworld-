814fddcb71dec7f73912208e9dd502056714660b
gisa chris <gisachris24@gmail.com>
2023-08-22 23:09:36 -0200

    main

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   root 'pages#hello'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
